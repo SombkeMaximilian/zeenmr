c13fed3de0224ee5b67887a058225f3e3209b7ae
Sombke Maximilian <sombkemaximilian@gmail.com>
2025-08-01 05:27:07 +0200

    Refactor: Reorder fields and methods of SpectralLinspace

--- zeenmr-core/zeenmr-spectrum/src/spectral_linspace.rs
+++ zeenmr-core/zeenmr-spectrum/src/spectral_linspace.rs
@@ -26,10 +26,10 @@ use serde::{Deserialize, Serialize};
     serde(rename_all = "camelCase")
 )]
 pub(crate) struct SpectralLinspace {
-    /// Frequency range of the spectrum in Hz.
-    frequency_range: (f64, f64),
     /// Spectrometer frequency in MHz.
     spectrometer_frequency: f64,
+    /// Frequency range of the spectrum in Hz.
+    frequency_range: (f64, f64),
     /// Number of points in the spectral axis.
     size: usize,
     /// Chemical shift reference.
@@ -50,8 +50,8 @@ impl SpectralLinspace {
     /// This function will return an error if the chemical shift reference index
     /// is out of bounds for the given size.
     pub(crate) fn new<T: Into<ShiftReference>>(
-        frequency_range: (f64, f64),
         spectrometer_frequency: f64,
+        frequency_range: (f64, f64),
         size: usize,
         reference: T,
     ) -> Result<Self> {
@@ -62,16 +62,11 @@ impl SpectralLinspace {
         Self::validate_reference_index(reference.index(), size)?;
 
         Ok(Self {
-            frequency_range,
             spectrometer_frequency,
+            frequency_range,
             size,
             reference,
         })
-    }
-
-    /// Returns the frequency range of the spectral axis in Hz.
-    pub(crate) fn range_hz(&self) -> (f64, f64) {
-        self.frequency_range
     }
 
     /// Returns the spectrometer frequency in MHz.
@@ -89,61 +84,9 @@ impl SpectralLinspace {
         &self.reference
     }
 
-    /// Sets the frequency range of the spectrum in Hz.
-    ///
-    /// The order of the range determines the direction of the spectral axis,
-    /// where the first value is the first point, and the second value is the
-    /// last point.
-    pub(crate) fn set_frequency_range(&mut self, frequency_range: (f64, f64)) -> Result<()> {
-        Self::validate_frequency_range(frequency_range)?;
-        self.frequency_range = frequency_range;
-
-        Ok(())
-    }
-
-    /// Sets the spectrometer frequency in MHz.
-    ///
-    /// This value is used to standardize the chemical shifts in the spectrum.
-    pub(crate) fn set_spectrometer_frequency(&mut self, spectrometer_frequency: f64) -> Result<()> {
-        Self::validate_spectrometer_frequency(spectrometer_frequency)?;
-        self.spectrometer_frequency = spectrometer_frequency;
-
-        Ok(())
-    }
-
-    /// Sets the number of points in the spectral axis.
-    ///
-    /// # Errors
-    ///
-    /// This function will return an error if the chemical shift reference index
-    /// is out of bounds for the new size.
-    pub(crate) fn set_size(&mut self, size: usize) -> Result<()> {
-        Self::validate_reference_index(self.reference.index(), size)?;
-        self.size = size;
-
-        Ok(())
-    }
-
-    /// Sets the chemical shift reference.
-    ///
-    /// # Errors
-    ///
-    /// This function will return an error if the new reference index is out of
-    /// bounds for the current size of the spectral axis.
-    pub(crate) fn set_shift_reference<T: Into<ShiftReference>>(
-        &mut self,
-        reference: T,
-    ) -> Result<()> {
-        let reference = reference.into();
-        if self.reference.index() >= reference.index() {
-            return Err(Error::reference_index_out_of_bounds(
-                reference.index(),
-                self.size,
-            ));
-        }
-        self.reference = reference;
-
-        Ok(())
+    /// Returns the frequency range of the spectral axis in Hz.
+    pub(crate) fn range_hz(&self) -> (f64, f64) {
+        self.frequency_range
     }
 
     /// Returns the chemical shift range of the spectral axis in ppm.
@@ -210,6 +153,62 @@ impl SpectralLinspace {
         (0..self.size).map(move |i| offset + step * i as f64)
     }
 
+    /// Sets the frequency range of the spectrum in Hz.
+    ///
+    /// The order of the range determines the direction of the spectral axis,
+    /// where the first value is the first point, and the second value is the
+    /// last point.
+    pub(crate) fn set_frequency_range(&mut self, frequency_range: (f64, f64)) -> Result<()> {
+        Self::validate_frequency_range(frequency_range)?;
+        self.frequency_range = frequency_range;
+
+        Ok(())
+    }
+
+    /// Sets the spectrometer frequency in MHz.
+    ///
+    /// This value is used to standardize the chemical shifts in the spectrum.
+    pub(crate) fn set_spectrometer_frequency(&mut self, spectrometer_frequency: f64) -> Result<()> {
+        Self::validate_spectrometer_frequency(spectrometer_frequency)?;
+        self.spectrometer_frequency = spectrometer_frequency;
+
+        Ok(())
+    }
+
+    /// Sets the number of points in the spectral axis.
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if the chemical shift reference index
+    /// is out of bounds for the new size.
+    pub(crate) fn set_size(&mut self, size: usize) -> Result<()> {
+        Self::validate_reference_index(self.reference.index(), size)?;
+        self.size = size;
+
+        Ok(())
+    }
+
+    /// Sets the chemical shift reference.
+    ///
+    /// # Errors
+    ///
+    /// This function will return an error if the new reference index is out of
+    /// bounds for the current size of the spectral axis.
+    pub(crate) fn set_shift_reference<T: Into<ShiftReference>>(
+        &mut self,
+        reference: T,
+    ) -> Result<()> {
+        let reference = reference.into();
+        if self.reference.index() >= reference.index() {
+            return Err(Error::reference_index_out_of_bounds(
+                reference.index(),
+                self.size,
+            ));
+        }
+        self.reference = reference;
+
+        Ok(())
+    }
     fn validate_frequency_range(frequency_range: (f64, f64)) -> Result<()> {
         match frequency_range.0.is_finite() && frequency_range.1.is_finite() {
             true => Ok(()),
@@ -250,11 +249,15 @@ mod tests {
     fn serialization_round_trip() {
         let frequency_range = (12000.0, 0.0);
         let spectrometer_frequency = 600.25;
-        let size = 2_usize.pow(15);
+        let spectrum_size = 2_usize.pow(15);
         let first_point = frequency_range.0 / spectrometer_frequency;
-        let linspace =
-            SpectralLinspace::new(frequency_range, spectrometer_frequency, size, first_point)
-                .unwrap();
+        let linspace = SpectralLinspace::new(
+            spectrometer_frequency,
+            frequency_range,
+            spectrum_size,
+            first_point,
+        )
+        .unwrap();
         let serialized = serde_json::to_string(&linspace).unwrap();
         let deserialized = serde_json::from_str::<SpectralLinspace>(&serialized).unwrap();
         assert_approx_eq!(
@@ -290,8 +293,8 @@ mod tests {
         let spectrum_size = 2_usize.pow(17);
         let first_point = frequency_range.0 / spectrometer_frequency;
         let linspace = SpectralLinspace::new(
-            frequency_range,
             spectrometer_frequency,
+            frequency_range,
             spectrum_size,
             first_point,
         )
