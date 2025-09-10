9648fa091483507391f72aa6ddf88c14be43219b
Sombke Maximilian <sombkemaximilian@gmail.com>
2025-09-10 19:52:09 +0200

    feat: add error impls

--- zeenmr-core/zeenmr-deconvolution/src/error.rs
+++ zeenmr-core/zeenmr-deconvolution/src/error.rs
@@ -49,3 +49,72 @@ pub enum Kind {
     /// might silently fail. This error avoids that issue.
     EmptySignalFreeRegion,
 }
+
+impl From<Kind> for Error {
+    fn from(kind: Kind) -> Self {
+        Self {
+            kind,
+            source: None,
+        }
+    }
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        self.source.as_ref().map(|e| e.as_ref() as _)
+    }
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let description = match self.kind() {
+            Kind::NoPeaksDetected => "no peaks detected",
+            Kind::EmptySignalRegion => "no peaks found in signal region",
+            Kind::EmptySignalFreeRegion => "no peaks found in signal-free region",
+        };
+
+        write!(f, "{description}")
+    }
+}
+
+impl Error {
+    /// Creates a [`NoPeaksDetected`] error.
+    ///
+    /// [`NoPeaksDetected`]: Kind::NoPeaksDetected
+    pub(crate) fn no_peaks_detected() -> Self {
+        Self {
+            kind: Kind::NoPeaksDetected,
+            source: None,
+        }
+    }
+
+    /// Creates an [`EmptySignalRegion`] error.
+    ///
+    /// [`EmptySignalRegion`]: Kind::EmptySignalRegion
+    pub(crate) fn empty_signal_region() -> Self {
+        Self {
+            kind: Kind::EmptySignalRegion,
+            source: None,
+        }
+    }
+
+    /// Creates an [`EmptySignalFreeRegion`] error.
+    ///
+    /// [`EmptySignalFreeRegion`]: Kind::EmptySignalFreeRegion
+    pub(crate) fn empty_signal_free_region() -> Self {
+        Self {
+            kind: Kind::EmptySignalFreeRegion,
+            source: None,
+        }
+    }
+
+    /// Returns the `Kind` of error that occurred.
+    pub fn kind(&self) -> &Kind {
+        &self.kind
+    }
+
+    /// Returns the source of the error, if any.
+    pub fn source(&self) -> Option<&Error> {
+        self.source.as_deref()
+    }
+}
