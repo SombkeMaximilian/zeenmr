561da430ab26cb6126d610874801b43431924086
Sombke Maximilian <sombkemaximilian@gmail.com>
2025-09-10 17:20:36 +0200

    docs: add Peak documentation

--- zeenmr-core/zeenmr-deconvolution/src/peak_finding/peak.rs
+++ zeenmr-core/zeenmr-deconvolution/src/peak_finding/peak.rs
@@ -1,4 +1,11 @@
 
+/// A 1D NMR peak, represented by its left bound, center, and right bound.
+///
+/// The left and right bounds are the points where the curvature changes from
+/// concave down to concave up (inflection points). The central point is where
+/// a local maximum occurs. When constructing this type, it must be ensured that
+/// `left < center < right`, and that the aforementioned curvature properties
+/// hold.
 #[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
 pub struct Peak {
     /// Left bound index of the peak.
