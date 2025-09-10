2bfe0cf2639d8a7f795de2b8dfe2bedb3e64c237
Sombke Maximilian <sombkemaximilian@gmail.com>
2025-09-10 18:16:13 +0200

    docs: add docs to FindPeaks trait

--- zeenmr-core/zeenmr-deconvolution/src/peak_finding/find_peaks.rs
+++ zeenmr-core/zeenmr-deconvolution/src/peak_finding/find_peaks.rs
@@ -2,10 +2,13 @@ use crate::peak_finding::Peak;
 use num_traits::Float;
 use zeenmr_spectrum::IndexRange;
 
+/// Trait for finding peaks in a smoothed signal.
 pub trait FindPeaks<F>
 where
     F: Float,
 {
+    /// Finds peaks in the given smoothed signal within the specified bounds,
+    /// optionally ignoring certain regions.
     fn find_peaks<I>(
         &self,
         smoothed: I,
