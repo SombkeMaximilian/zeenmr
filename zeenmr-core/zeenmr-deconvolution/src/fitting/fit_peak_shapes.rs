9929e019fc46ad65f02af09076383c6f4b1766dc
Sombke Maximilian <sombkemaximilian@gmail.com>
2025-09-14 03:41:51 +0200

    fix: update trait bounds and return types of FitPeakShapes trait

--- zeenmr-core/zeenmr-deconvolution/src/fitting/fit_peak_shapes.rs
+++ zeenmr-core/zeenmr-deconvolution/src/fitting/fit_peak_shapes.rs
@@ -1,23 +1,27 @@
+use crate::peak_finding::Peak;
 use zeenmr_peakshape::PeakShape;
 use zeenmr_spectrum::Spectrum;
 
 #[cfg(feature = "rayon")]
-use rayon::iter::{IntoParallelIterator, ParallelIterator};
+use rayon::iter::IndexedParallelIterator;
 
+/// Trait for fitting peak shapes to a spectrum given a set of detected peaks.
 pub trait FitPeakShapes<P>
 where
-    P: PeakShape,
+    P: PeakShape + Send + Sync,
 {
+    /// Fits peak shapes to the spectrum using the provided peaks.
     fn fit_peak_shapes<I>(&self, spectrum: &Spectrum, peaks: I) -> impl Iterator<Item = P>
     where
-        I: Iterator<Item = usize>;
+        I: IntoIterator<Item = Peak>;
 
+    /// Fits peak shapes to the spectrum in parallel using the provided peaks.
     #[cfg(feature = "rayon")]
     fn par_fit_peak_shapes<I>(
         &self,
         spectrum: &Spectrum,
         peaks: I,
-    ) -> impl ParallelIterator<Item = P>
+    ) -> impl IndexedParallelIterator<Item = P>
     where
-        I: IntoParallelIterator<Item = usize>;
+        I: IntoIterator<Item = Peak>;
 }
