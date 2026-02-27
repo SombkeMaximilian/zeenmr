e4bb0888bf038a00b5da51a4a7a8b6db6d884a0d
Sombke Maximilian <sombkemaximilian@gmail.com>
2026-02-27 06:48:37 +0100

    refactor: remove version variant of Value enum

--- zeenmr-core/zeenmr-parse/src/jcampdx/data/value.rs
+++ zeenmr-core/zeenmr-parse/src/jcampdx/data/value.rs
@@ -8,8 +8,6 @@ pub enum Value {
     Integer(i64),
     /// Float values.
     Float(f64),
-    /// Version values with two or three parts.
-    Version(u32, u32, Option<u32>),
     /// Everything else is treated as a string.
     String(String),
     /// Arrays of values.
