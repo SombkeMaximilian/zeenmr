8c73787f04464220a555860332f353f56bb89e73
Sombke Maximilian <sombkemaximilian@gmail.com>
2026-03-11 13:28:22 +0100

    feat: table parser skeleton

--- zeenmr-core/zeenmr-parse/src/jcampdx/tabulation/parser.rs
+++ zeenmr-core/zeenmr-parse/src/jcampdx/tabulation/parser.rs
@@ -1,1 +1,41 @@
+use crate::Cursor;
+use crate::jcampdx::tabulation::{GroupToken, TabulatedBlockBuilder};
+use logos::{Lexer, Logos};
 
+/// Parser for tables row-major, grouped format.
+#[derive(Debug)]
+pub(crate) struct TableParser<'source> {
+    /// Lexer for the table format.
+    lexer: Lexer<'source, GroupToken>,
+    /// Table being constructed.
+    builder: TabulatedBlockBuilder,
+}
+
+impl<'source> From<&'source str> for TableParser<'source> {
+    fn from(value: &'source str) -> Self {
+        Self {
+            lexer: GroupToken::lexer(value),
+            builder: TabulatedBlockBuilder::default(),
+        }
+    }
+}
+
+impl<'source, T> From<Lexer<'source, T>> for TableParser<'source>
+where
+    T: Logos<'source, Source = str> + Clone,
+    T::Extras: Clone + Into<Cursor>,
+{
+    fn from(value: Lexer<'source, T>) -> Self {
+        Self {
+            lexer: value.morph(),
+            builder: TabulatedBlockBuilder::default(),
+        }
+    }
+}
+
+impl<'source> TableParser<'source> {
+    /// Recovers the [`Lexer`] from the `TableParser`.
+    pub(crate) fn into_lexer(self) -> Lexer<'source, GroupToken> {
+        self.lexer
+    }
+}
