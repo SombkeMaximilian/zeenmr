e1c42575cd981f61897662cb7a7e67bf2c2fa7fe
Sombke Maximilian <sombkemaximilian@gmail.com>
2026-03-20 04:59:17 +0100

    feat: progressed skeleton of the tabulation parser

--- zeenmr-core/zeenmr-parse/src/jcampdx/tabulation/parser.rs
+++ zeenmr-core/zeenmr-parse/src/jcampdx/tabulation/parser.rs
@@ -1,6 +1,17 @@
 use crate::Cursor;
-use crate::jcampdx::tabulation::{GroupToken, TabulatedBlockBuilder};
+use crate::jcampdx::tabulation::error::{Error, Result};
+use crate::jcampdx::tabulation::{GroupToken, TabulatedBlock, TabulatedBlockBuilder};
 use logos::{Lexer, Logos};
+
+/// Expected layout of the lines.
+#[derive(Copy, Clone, Eq, PartialEq, Debug, Default)]
+enum LineLayout {
+    /// Multiple groups per line.
+    #[default]
+    MultiGroup,
+    /// One group per line.
+    SingleGroup,
+}
 
 /// Parser for tables row-major, grouped format.
 #[derive(Debug)]
@@ -9,6 +20,12 @@ pub(crate) struct TableParser<'source> {
     lexer: Lexer<'source, GroupToken>,
     /// Table being constructed.
     builder: TabulatedBlockBuilder,
+    /// Expected layout of the lines.
+    layout: LineLayout,
+    /// Expected size of the groups.
+    group_size: usize,
+    /// Start of string enclosed by angle brackets.
+    string_start: Option<usize>,
 }
 
 impl<'source> From<&'source str> for TableParser<'source> {
@@ -16,6 +33,9 @@ impl<'source> From<&'source str> for TableParser<'source> {
         Self {
             lexer: GroupToken::lexer(value),
             builder: TabulatedBlockBuilder::default(),
+            layout: LineLayout::default(),
+            group_size: 0,
+            string_start: None,
         }
     }
 }
@@ -29,6 +49,9 @@ where
         Self {
             lexer: value.morph(),
             builder: TabulatedBlockBuilder::default(),
+            layout: LineLayout::default(),
+            group_size: 0,
+            string_start: None,
         }
     }
 }
@@ -37,5 +60,43 @@ impl<'source> TableParser<'source> {
     /// Recovers the [`Lexer`] from the `TableParser`.
     pub(crate) fn into_lexer(self) -> Lexer<'source, GroupToken> {
         self.lexer
+    }
+
+    pub(crate) fn set_multi_group_identifiers(&mut self, identifiers: Vec<String>) {
+        self.layout = LineLayout::MultiGroup;
+        self.group_size = identifiers.len();
+        self.builder.set_columns(identifiers);
+    }
+
+    pub(crate) fn set_single_group_identifiers(&mut self, identifiers: Vec<String>) {
+        self.layout = LineLayout::SingleGroup;
+        self.group_size = identifiers.len();
+        self.builder.set_columns(identifiers);
+    }
+
+    /// Tabulates the source into a `TabulatedBlock`.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error for fatal tabulation failures, such as invalid
+    /// literals or an inconsistent number of columns.
+    pub(crate) fn tabulate_source(&mut self) -> Result<TabulatedBlock> {
+        debug_assert!(self.group_size > 0);
+        while let Some(token) = self.lexer.next().transpose()? {
+            match token {
+                GroupToken::Checkpoint => {}
+                GroupToken::Comma => {}
+                GroupToken::Semicolon => {}
+                GroupToken::OpenParenthesis => {}
+                GroupToken::CloseParenthesis => {}
+                GroupToken::OpenAngle => {}
+                GroupToken::CloseAngle => {}
+                GroupToken::Numeric => {}
+                GroupToken::String => {}
+                GroupToken::End => {}
+            }
+        }
+
+        Ok(std::mem::take(&mut self.builder).finalize())
     }
 }
