a1db0b4476fa5b97fefbc1a3463ce1b2c632e177
Sombke Maximilian <sombkemaximilian@gmail.com>
2026-06-05 06:15:13 +0200

    refactor: add extensible bruker binary reader

--- zeenmr-core/zeenmr-parse/src/bruker.rs
+++ zeenmr-core/zeenmr-parse/src/bruker.rs
@@ -1,7 +1,7 @@
 //! Bruker NMR data parsing.
 
 mod binary;
-pub(crate) use binary::{DataType, Endian, read_bruker_binary};
+pub(crate) use binary::BrukerBinaryReader;
 
 mod parser;
 pub(crate) use parser::Parser;
--- zeenmr-core/zeenmr-parse/src/bruker/binary.rs
+++ zeenmr-core/zeenmr-parse/src/bruker/binary.rs
@@ -1,71 +1,74 @@
-use byteorder::{BigEndian, LittleEndian, ReadBytesExt};
+use crate::data::DataTable;
+use byteorder::{ByteOrder, ReadBytesExt};
 use std::fs::File;
-use std::io::Result;
+use std::io::{self, BufReader};
+use std::marker::PhantomData;
 use std::path::Path;
 
-/// Data type of the raw data. Extracted from the `acqus`/`procs` file.
-///
-/// | DTYPA/P | Type |
-/// | ------- | ---- |
-/// | 0       | i32  |
-/// | 1       | f64  |
-#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
-pub(crate) enum DataType {
-    /// Data stored as 32-bit signed integers.
-    I32,
-    /// Data stored as 64-bit floating point numbers.
-    F64,
+/// Reader adapter for Bruker binary files.
+#[derive(Debug)]
+pub(crate) struct BrukerBinaryReader<E> {
+    /// Reader for the main file.
+    reader: BufReader<File>,
+    /// Endianness of the data in the file.
+    endian: PhantomData<E>,
 }
 
-/// Endianness of the raw data. Extracted from the `acqus`/`procs` file.
-///
-/// | BYTORDA/P | Endianness |
-/// | --------- | ---------- |
-/// | 0         | Little     |
-/// | 1         | Big        |
-#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
-pub(crate) enum Endian {
-    /// Little-endian byte order.
-    Little,
-    /// Big-endian byte order.
-    Big,
+impl<E> BrukerBinaryReader<E> {
+    /// Attempts to open a Bruker binary file in read-only mode.
+    pub(crate) fn new<P: AsRef<Path>>(path: P) -> io::Result<Self> {
+        Ok(Self {
+            reader: BufReader::new(File::open(path)?),
+            endian: PhantomData,
+        })
+    }
 }
 
-/// Reads a Bruker binary file (e.g., `1r`, `1i`, `fid`) and returns its
-/// contents as a collection of `f64` values.
-pub(crate) fn read_bruker_binary<P>(
-    path: P,
-    size: usize,
-    data_type: DataType,
-    endian: Endian,
-    exponent: i32,
-) -> Result<Vec<f64>>
+impl<E> BrukerBinaryReader<E>
 where
-    P: AsRef<Path>,
+    E: ByteOrder,
 {
-    let mut file = File::open(path)?;
+    /// Attempts to read complex `i32` values from the file, and returns a data
+    /// table with "R" and "I" columns.
+    ///
+    /// # Errors
+    ///
+    /// Returns the same errors as [`io::Read::read_exact`].
+    pub(crate) fn read_i32_complex(self, n: usize, exp: u32) -> io::Result<DataTable<'static>> {
+        let mut table = DataTable::new();
+        let mut complex = vec![0_i32; n];
+        self.reader
+            .into_inner()
+            .read_i32_into::<E>(&mut complex)?;
+        let (real, imag) = complex
+            .chunks_exact(2)
+            .map(|c| ((c[0] as i64) << exp, (c[1] as i64) << exp))
+            .unzip::<i64, i64, Vec<i64>, Vec<i64>>();
+        table.insert("R".into(), real.into());
+        table.insert("I".into(), imag.into());
 
-    match data_type {
-        DataType::I32 => {
-            let mut data = vec![0_i32; size];
-            match endian {
-                Endian::Little => file.read_i32_into::<LittleEndian>(&mut data)?,
-                Endian::Big => file.read_i32_into::<BigEndian>(&mut data)?,
-            };
+        Ok(table)
+    }
 
-            Ok(data
-                .into_iter()
-                .map(|x| (x as f64) * 2_f64.powi(exponent))
-                .collect())
-        }
-        DataType::F64 => {
-            let mut data = vec![0_f64; size];
-            match endian {
-                Endian::Little => file.read_f64_into::<LittleEndian>(&mut data)?,
-                Endian::Big => file.read_f64_into::<BigEndian>(&mut data)?,
-            };
+    /// Attempts to read complex `f64` values from the file, and returns a data
+    /// table with "R" and "I" columns.
+    ///
+    /// # Errors
+    ///
+    /// Returns the same errors as [`io::Read::read_exact`].
+    pub(crate) fn read_f64_complex(self, n: usize) -> io::Result<DataTable<'static>> {
+        let mut table = DataTable::new();
+        let mut complex = vec![0_f64; n];
+        self.reader
+            .into_inner()
+            .read_f64_into::<E>(&mut complex)?;
+        let (real, imag) = complex
+            .chunks_exact(2)
+            .map(|c| (c[0], c[1]))
+            .unzip::<f64, f64, Vec<f64>, Vec<f64>>();
+        table.insert("R".into(), real.into());
+        table.insert("I".into(), imag.into());
 
-            Ok(data)
-        }
+        Ok(table)
     }
 }
