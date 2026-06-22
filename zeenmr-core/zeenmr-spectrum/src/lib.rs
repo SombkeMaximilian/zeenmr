efb0e37e6566de2b865f432b0b5e3bfa2f680501
Sombke Maximilian <sombkemaximilian@gmail.com>
2026-06-22 21:49:55 +0200

    chore: prepare for clean rewrite of Spectrum
    - the previous implementation had a lot of badly informed choices as a result of being the first building block
    - nucleus and reference are the most keepable parts

--- zeenmr-core/zeenmr-spectrum/src/lib.rs
+++ zeenmr-core/zeenmr-spectrum/src/lib.rs
@@ -8,17 +8,3 @@ pub use nucleus::Nucleus;
 
 mod reference;
 pub use reference::ShiftReference;
-
-mod spectral_linspace;
-pub(crate) use spectral_linspace::SpectralLinspace;
-
-mod spectral_range;
-pub use spectral_range::{
-    ChemicalShiftRange, FrequencyRange, IndexRange, RelativeRange, SpectralRange,
-    TryFromIndexRange, TryIntoIndexRange,
-};
-
-mod spectrum;
-pub use spectrum::Spectrum;
-
-pub mod error;
