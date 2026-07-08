1a7e1017f8a9ecb0d1d9adc8495cae5519e060c4
Sombke Maximilian <sombkemaximilian@gmail.com>
2026-07-08 08:43:52 +0200

    feat: pruning of bad peaks

--- zeenmr-core/zeenmr-deconvolution/src/fitting/iterative_refinement.rs
+++ zeenmr-core/zeenmr-deconvolution/src/fitting/iterative_refinement.rs
@@ -20,28 +20,30 @@ use serde::{Deserialize, Serialize};
 /// Trait for estimating parameters of a peak shape from three data points.
 pub trait ThreePointStencil<T> {
     /// Estimate the parameters of the peak shape from three data points.
-    fn estimate_parameters(x: (T, T, T), y: (T, T, T)) -> Self;
+    fn estimate_parameters(x: [T; 3], y: [T; 3]) -> Self;
 }
 
 impl<T> ThreePointStencil<T> for Lorentzian<T>
 where
     T: Float,
 {
-    fn estimate_parameters(x: (T, T, T), y: (T, T, T)) -> Self {
+    fn estimate_parameters(x: [T; 3], y: [T; 3]) -> Self {
         let half = T::one() / (T::one() + T::one());
 
-        let numerator = x.0.powi(2) * y.0 * (y.1 - y.2)
-            + x.1.powi(2) * y.1 * (y.2 - y.0)
-            + x.2.powi(2) * y.2 * (y.0 - y.1);
+        let numerator = x[0].powi(2) * y[0] * (y[1] - y[2])
+            + x[1].powi(2) * y[1] * (y[2] - y[0])
+            + x[2].powi(2) * y[2] * (y[0] - y[1]);
         let denominator =
-            y.0 * y.1 * (x.0 - x.1) + y.1 * y.2 * (x.1 - x.2) + y.2 * y.0 * (x.2 - x.0);
+            y[0] * y[1] * (x[0] - x[1]) + y[1] * y[2] * (x[1] - x[2]) + y[2] * y[0] * (x[2] - x[0]);
         let center = half * numerator / denominator;
 
-        let left = (y.0 * (x.0 - center).powi(2) - y.1 * (x.1 - center).powi(2)) / (y.1 - y.0);
-        let right = (y.1 * (x.1 - center).powi(2) - y.2 * (x.2 - center).powi(2)) / (y.2 - y.1);
-        let scale2 = (half * (left + right)).max(T::epsilon());
+        let left =
+            (y[0] * (x[0] - center).powi(2) - y[1] * (x[1] - center).powi(2)) / (y[1] - y[0]);
+        let right =
+            (y[1] * (x[1] - center).powi(2) - y[2] * (x[2] - center).powi(2)) / (y[2] - y[1]);
+        let scale2 = half * (left + right);
 
-        let amp_scale = y.1 * (scale2 + (x.1 - center).powi(2));
+        let amp_scale = y[1] * (scale2 + (x[1] - center).powi(2));
 
         Lorentzian::new(amp_scale, scale2, center)
     }
@@ -52,9 +54,9 @@ where
 #[derive(Clone, Debug)]
 struct ReducedSpectrum<T> {
     /// Chemical shifts that are part of the peaks, in ppm.
-    shifts: Vec<T>,
+    shifts: Vec<[T; 3]>,
     /// Intensity values that are part of the peaks.
-    intensities: Vec<T>,
+    intensities: Vec<[T; 3]>,
 }
 
 impl<T> ReducedSpectrum<T>
@@ -70,18 +72,27 @@ where
         let len = spectrum.intensities().len();
         let len_as_t = T::from(len).expect("conversion from usize to T must never fail");
         let axis = spectrum.axis();
+        let index_to_shift = move |index: usize| {
+            let index_as_t = T::from(index).expect("conversion from usize to T must never fail");
+
+            axis.rel_to_shift(index_as_t / len_as_t)
+                .expect("index should be less than len")
+        };
         let (shifts, intensities) = peaks
             .iter()
             .filter(|peak| peak.right < len)
-            .flat_map(|peak| [peak.left, peak.center, peak.right])
-            .map(|index| {
-                let index_as_t =
-                    T::from(index).expect("conversion from usize to T must never fail");
-
+            .map(|peak| {
                 (
-                    axis.rel_to_shift(index_as_t / len_as_t)
-                        .expect("indices cannot be greater than len"),
-                    spectrum.intensities()[index],
+                    [
+                        index_to_shift(peak.left),
+                        index_to_shift(peak.center),
+                        index_to_shift(peak.right),
+                    ],
+                    [
+                        spectrum.intensities()[peak.left],
+                        spectrum.intensities()[peak.center],
+                        spectrum.intensities()[peak.right],
+                    ],
                 )
             })
             .unzip::<_, _, Vec<_>, Vec<_>>();
@@ -95,12 +106,12 @@ where
     /// Returns an iterator over the peak stencils in the reduced spectrum.
     fn stencils(&self) -> impl Iterator<Item = PeakStencil<T>> {
         self.shifts
-            .chunks(3)
-            .zip(self.intensities.chunks(3))
+            .iter()
+            .zip(self.intensities.iter())
             .map(|(shifts, intensities)| {
                 let mut stencil = PeakStencil {
-                    shifts: (shifts[0], shifts[1], shifts[2]),
-                    intensities: (intensities[0], intensities[1], intensities[2]),
+                    shifts: [shifts[0], shifts[1], shifts[2]],
+                    intensities: [intensities[0], intensities[1], intensities[2]],
                 };
                 stencil.mirror_shoulder();
 
@@ -112,9 +123,9 @@ where
 #[derive(Copy, Clone, Debug)]
 struct PeakStencil<T> {
     /// Chemical shifts of the three points in ppm.
-    shifts: (T, T, T),
+    shifts: [T; 3],
     /// Intensity values of the three points.
-    intensities: (T, T, T),
+    intensities: [T; 3],
 }
 
 impl<T> PeakStencil<T>
@@ -132,19 +143,18 @@ where
     /// stencil is working with data that has a peak-like shape.
     fn mirror_shoulder(&mut self) {
         let two = T::one() + T::one();
-
-        let increasing =
-            self.intensities.0 <= self.intensities.1 && self.intensities.1 <= self.intensities.2;
-        let decreasing =
-            self.intensities.0 >= self.intensities.1 && self.intensities.1 >= self.intensities.2;
+        let increasing = self.intensities[0] <= self.intensities[1]
+            && self.intensities[1] <= self.intensities[2];
+        let decreasing = self.intensities[0] >= self.intensities[1]
+            && self.intensities[1] >= self.intensities[2];
         match (increasing, decreasing) {
             (true, _) => {
-                self.intensities.2 = self.intensities.0;
-                self.shifts.2 = two * self.shifts.1 - self.shifts.0;
+                self.intensities[2] = self.intensities[0];
+                self.shifts[2] = two * self.shifts[1] - self.shifts[0];
             }
             (_, true) => {
-                self.intensities.0 = self.intensities.2;
-                self.shifts.0 = two * self.shifts.1 - self.shifts.2;
+                self.intensities[0] = self.intensities[2];
+                self.shifts[0] = two * self.shifts[1] - self.shifts[2];
             }
             _ => {}
         };
@@ -181,37 +191,45 @@ where
     where
         S: Array1D<Elem = T>,
     {
-        let reduced_spectrum = ReducedSpectrum::new(spectrum, peaks);
+        let mut reduced_spectrum = ReducedSpectrum::new(spectrum, peaks);
         let mut stencils = reduced_spectrum.stencils().collect::<Vec<_>>();
         let mut peak_shapes = stencils
             .iter()
             .map(|stencil| P::estimate_parameters(stencil.shifts, stencil.intensities))
             .collect::<Vec<_>>();
         for _ in 0..self.iterations {
-            let superpositions = reduced_spectrum
+            prune(
+                &mut reduced_spectrum.shifts,
+                &mut reduced_spectrum.intensities,
+                &mut stencils,
+                &mut peak_shapes,
+            );
+            if peak_shapes.is_empty() {
+                break;
+            }
+            let ratios = reduced_spectrum
                 .shifts
+                .as_flattened()
                 .iter()
                 .copied()
-                .superposition(&peak_shapes);
-            let ratios = reduced_spectrum
-                .intensities
-                .iter()
-                .zip(superpositions)
-                .map(|(&intensity, superposition)| intensity / superposition)
+                .superposition(&peak_shapes)
+                .zip(reduced_spectrum.intensities.as_flattened().iter())
+                .map(|(superposition, &intensity)| intensity / superposition)
                 .collect::<Vec<_>>();
             for (stencil, ratios) in stencils.iter_mut().zip(ratios.chunks(3)) {
-                stencil.intensities.0 = stencil.intensities.0 * ratios[0];
-                stencil.intensities.1 = stencil.intensities.1 * ratios[1];
-                stencil.intensities.2 = stencil.intensities.2 * ratios[2];
+                stencil.intensities[0] = stencil.intensities[0] * ratios[0];
+                stencil.intensities[1] = stencil.intensities[1] * ratios[1];
+                stencil.intensities[2] = stencil.intensities[2] * ratios[2];
                 stencil.mirror_shoulder();
             }
-            for (peak_shape, stencil) in peak_shapes.iter_mut().zip(stencils.iter()) {
-                *peak_shape = P::estimate_parameters(stencil.shifts, stencil.intensities);
-            }
+            peak_shapes
+                .iter_mut()
+                .zip(stencils.iter())
+                .for_each(|(p, stencil)| {
+                    *p = P::estimate_parameters(stencil.shifts, stencil.intensities);
+                });
         }
-        peak_shapes.retain(|peak_shape| {
-            peak_shape.is_valid() && peak_shape.is_significant(crate::precision())
-        });
+        peak_shapes.retain(|p| p.is_valid() && p.is_significant(crate::precision()));
 
         peak_shapes
     }
@@ -227,41 +245,45 @@ where
     where
         S: Array1D<Elem = T>,
     {
-        let reduced_spectrum = ReducedSpectrum::new(spectrum, peaks);
+        let mut reduced_spectrum = ReducedSpectrum::new(spectrum, peaks);
         let mut stencils = reduced_spectrum.stencils().collect::<Vec<_>>();
         let mut peak_shapes = stencils
             .iter()
             .map(|stencil| P::estimate_parameters(stencil.shifts, stencil.intensities))
             .collect::<Vec<_>>();
         for _ in 0..self.iterations {
-            let superpositions = reduced_spectrum
+            prune(
+                &mut reduced_spectrum.shifts,
+                &mut reduced_spectrum.intensities,
+                &mut stencils,
+                &mut peak_shapes,
+            );
+            if peak_shapes.is_empty() {
+                break;
+            }
+            let ratios = reduced_spectrum
                 .shifts
+                .as_flattened()
                 .par_iter()
                 .copied()
                 .superposition(&peak_shapes)
-                .collect::<Vec<_>>();
-            let ratios = reduced_spectrum
-                .intensities
-                .iter()
-                .zip(superpositions)
-                .map(|(&intensity, superposition)| intensity / superposition)
+                .zip(reduced_spectrum.intensities.as_flattened().par_iter())
+                .map(|(superposition, &intensity)| intensity / superposition)
                 .collect::<Vec<_>>();
             for (stencil, ratios) in stencils.iter_mut().zip(ratios.chunks(3)) {
-                stencil.intensities.0 = stencil.intensities.0 * ratios[0];
-                stencil.intensities.1 = stencil.intensities.1 * ratios[1];
-                stencil.intensities.2 = stencil.intensities.2 * ratios[2];
+                stencil.intensities[0] = stencil.intensities[0] * ratios[0];
+                stencil.intensities[1] = stencil.intensities[1] * ratios[1];
+                stencil.intensities[2] = stencil.intensities[2] * ratios[2];
                 stencil.mirror_shoulder();
             }
             peak_shapes
                 .par_iter_mut()
                 .zip(stencils.par_iter())
-                .for_each(|(peak_shape, stencil)| {
-                    *peak_shape = P::estimate_parameters(stencil.shifts, stencil.intensities);
+                .for_each(|(p, stencil)| {
+                    *p = P::estimate_parameters(stencil.shifts, stencil.intensities);
                 });
         }
-        peak_shapes.retain(|peak_shape| {
-            peak_shape.is_valid() && peak_shape.is_significant(crate::precision())
-        });
+        peak_shapes.retain(|p| p.is_valid() && p.is_significant(crate::precision()));
 
         peak_shapes
     }
@@ -279,6 +301,37 @@ impl<P> IterativeRefinement<P> {
         Self {
             iterations,
             peak_shape: PhantomData,
+        }
+    }
+}
+
+/// Prunes the loop components by whether the associated peak shape is
+/// degenerate.
+///
+/// # Panics
+///
+/// Panics in debug mode if the loop components have differing lengths.
+fn prune<T, P>(
+    shifts: &mut Vec<[T; 3]>,
+    intensities: &mut Vec<[T; 3]>,
+    stencils: &mut Vec<PeakStencil<T>>,
+    peak_shapes: &mut Vec<P>,
+) where
+    T: Float,
+    P: PeakShape<T>,
+{
+    debug_assert_eq!(shifts.len(), intensities.len());
+    debug_assert_eq!(intensities.len(), stencils.len());
+    debug_assert_eq!(stencils.len(), peak_shapes.len());
+    let mut curr = 0;
+    while curr < peak_shapes.len() {
+        if peak_shapes[curr].is_valid() && peak_shapes[curr].is_significant(crate::precision()) {
+            curr += 1;
+        } else {
+            shifts.swap_remove(curr);
+            intensities.swap_remove(curr);
+            stencils.swap_remove(curr);
+            peak_shapes.swap_remove(curr);
         }
     }
 }
