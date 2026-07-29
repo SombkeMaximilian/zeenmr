c4504906f2a1f1834d76ed0674012c754543fde3
Sombke Maximilian <sombkemaximilian@gmail.com>
2026-07-29 02:26:14 +0200

    feat: array index, shape, strides and layout

--- zeenmr-core/zeenmr-spectrum/src/intensity_array.rs
+++ zeenmr-core/zeenmr-spectrum/src/intensity_array.rs
@@ -4,5 +4,6 @@ mod storage;
 pub use storage::{Storage, StorageMut, StorageOwned};
 
 mod dimension;
+pub use dimension::{DimIndex, Dimension, StaticDim, DynDim, Shape, ArrayIndex, Strides, Layout};
 
 pub mod diagnostic_1d;
--- zeenmr-core/zeenmr-spectrum/src/intensity_array/dimension.rs
+++ zeenmr-core/zeenmr-spectrum/src/intensity_array/dimension.rs
@@ -1,16 +1,16 @@
 use std::ops::{Deref, DerefMut};
 
-/// Maximum number of static dimensions.
+/// Maximum number of non-heap dimensions in the dynamic case.
 ///
-/// Up to 3D NMR can be statically supported.
-const MAX_STATIC_DIMS: usize = 3;
+/// Limited to 3D to keep the most common cases as lean as possible.
+const MAX_INLINE_RANK: usize = 3;
 
 /// Array dimension index.
 ///
 /// An array generally has `N` dimensions. This type encapsulates the index `i`
 /// of such a dimension with `0 <= i < N`.
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
-pub struct DimIndex(usize);
+pub struct DimIndex(pub usize);
 
 impl DimIndex {
     /// Returns the index of the dimension.
@@ -21,44 +21,98 @@ impl DimIndex {
     }
 }
 
-pub trait Dimensions: Clone + Eq + PartialEq + AsRef<[usize]> + AsMut<[usize]> {
+/// Abstraction for multidimensional quantities.
+pub trait Dimension: Clone + Eq + Send + Sync {
     /// Compile time constant rank, if available.
     const RANK: Option<usize>;
 
     /// Returns a value of the given rank with all entries zero, or `None` if
     /// this type cannot represent it.
-    fn zeroed(rank: usize) -> Option<Self>;
+    fn zero(rank: usize) -> Option<Self>;
+
+    /// Returns the equivalent instance of `self`, or `None` if this type cannot
+    /// represent `other`'s rank.
+    fn from_dimension<D>(other: &D) -> Option<Self>
+    where
+        D: Dimension;
+
+    /// Returns a slice containing all dimensions of this quantity.
+    fn as_slice(&self) -> &[usize];
+
+    /// Returns a mutable slice containing all dimensions of this quantity.
+    fn as_mut_slice(&mut self) -> &mut [usize];
 
     /// Returns the rank of `self`.
+    #[inline]
     fn rank(&self) -> usize {
-        self.as_ref().len()
+        self.as_slice().len()
     }
 }
 
-impl<const N: usize> Dimensions for [usize; N] {
+/// Multidimensional quantity with a size determined at compile-time.
+// TODO: Eq, PartialEq, Ord, PartialOrd and Hash need to be manually written
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
+pub struct StaticDim<const N: usize>([usize; N]);
+
+impl<const N: usize> From<[usize; N]> for StaticDim<N> {
+    fn from(value: [usize; N]) -> Self {
+        Self(value)
+    }
+}
+
+impl<const N: usize> Dimension for StaticDim<N> {
     const RANK: Option<usize> = Some(N);
 
-    fn zeroed(rank: usize) -> Option<Self> {
+    #[inline]
+    fn zero(rank: usize) -> Option<Self> {
         if rank == N {
-            Some([0; N])
+            Some(StaticDim([0; N]))
         } else {
             None
         }
     }
 
+    fn from_dimension<D>(other: &D) -> Option<Self>
+    where
+        D: Dimension,
+    {
+        if other.rank() == N {
+            other.as_slice().try_into().map(StaticDim).ok()
+        } else {
+            None
+        }
+    }
+
+    #[inline]
+    fn as_slice(&self) -> &[usize] {
+        self.0.as_ref()
+    }
+
+    #[inline]
+    fn as_mut_slice(&mut self) -> &mut [usize] {
+        self.0.as_mut()
+    }
+
+    #[inline]
     fn rank(&self) -> usize {
         N
     }
 }
 
-#[derive(Clone, Debug)]
+/// Multidimensional quantity representation with a size determined at runtime.
+// TODO: Eq, PartialEq, Ord, PartialOrd and Hash need to be manually written
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
 enum DynDimInner {
-    Stack(StaticLen, [usize; MAX_STATIC_DIMS]),
+    /// On the stack for up to [`MAX_INLINE_RANK`].
+    Stack(StaticLen, [usize; MAX_INLINE_RANK]),
+    /// On the heap for higher dimensions.
     Heap(Vec<usize>),
 }
 
+/// `u8` bounded to `[0, MAX_INLINE_RANK]`.
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Default)]
-#[repr(usize)]
+#[repr(u8)]
+#[allow(missing_docs)]
 enum StaticLen {
     #[default]
     Zero = 0,
@@ -69,8 +123,8 @@ enum StaticLen {
 
 impl StaticLen {
     /// Returns the equivalent `StaticLen`, or `None` if it exceeds
-    /// `MAX_STATIC_DIMS`.
-    fn from_usize(n: usize) -> Option<Self> {
+    /// [`MAX_INLINE_RANK`].
+    fn from_u8(n: u8) -> Option<Self> {
         match n {
             0 => Some(Self::Zero),
             1 => Some(Self::One),
@@ -103,16 +157,17 @@ impl DerefMut for DynDimInner {
 
 impl Default for DynDimInner {
     fn default() -> Self {
-        Self::Stack(StaticLen::Zero, [0; MAX_STATIC_DIMS])
+        Self::Stack(StaticLen::Zero, [0; MAX_INLINE_RANK])
     }
 }
 
 impl DynDimInner {
+    /// Creates a new dynamic dimension from an array.
     fn from_array<const N: usize>(value: [usize; N]) -> Self {
-        if N <= MAX_STATIC_DIMS {
-            let len = StaticLen::from_usize(N)
+        if N <= MAX_INLINE_RANK {
+            let len = StaticLen::from_u8(N as u8)
                 .expect("above `if` should make this infallible");
-            let mut dims = [0; MAX_STATIC_DIMS];
+            let mut dims = [0; MAX_INLINE_RANK];
             dims[..value.len()].copy_from_slice(&value);
 
             Self::Stack(len, dims)
@@ -121,19 +176,247 @@ impl DynDimInner {
         }
     }
 
-    fn from_vec(value: Vec<usize>) -> Self {
-        if value.len() <= MAX_STATIC_DIMS {
-            let len = StaticLen::from_usize(value.len())
+    /// Creates a new dynamic dimension from a slice.
+    fn from_slice<S>(value: S) -> Self
+    where
+        S: AsRef<[usize]> + Into<Vec<usize>>,
+    {
+        if value.as_ref().len() <= MAX_INLINE_RANK {
+            let len = StaticLen::from_u8(value.as_ref().len() as u8)
                 .expect("above `if` should make this infallible");
-            let mut dims = [0; MAX_STATIC_DIMS];
-            dims[..value.len()].copy_from_slice(&value);
+            let mut dims = [0; MAX_INLINE_RANK];
+            dims[..value.as_ref().len()].copy_from_slice(value.as_ref());
 
             Self::Stack(len, dims)
         } else {
-            Self::Heap(value)
+            Self::Heap(value.into())
         }
     }
 }
 
-#[derive(Clone, Debug)]
+/// Multidimensional quantity with a size determined at runtime.
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
 pub struct DynDim(DynDimInner);
+
+impl<const N: usize> From<[usize; N]> for DynDim {
+    fn from(value: [usize; N]) -> Self {
+        Self(DynDimInner::from_array(value))
+    }
+}
+
+impl From<&[usize]> for DynDim {
+    fn from(value: &[usize]) -> Self {
+        Self(DynDimInner::from_slice(value))
+    }
+}
+
+impl From<Vec<usize>> for DynDim {
+    fn from(value: Vec<usize>) -> Self {
+        Self(DynDimInner::from_slice(value))
+    }
+}
+
+impl Dimension for DynDim {
+    const RANK: Option<usize> = None;
+
+    fn zero(rank: usize) -> Option<Self> {
+        if rank <= MAX_INLINE_RANK {
+            let len = StaticLen::from_u8(rank as u8)
+                .expect("above `if` should make this infallible");
+
+            Some(Self(DynDimInner::Stack(len, [0; MAX_INLINE_RANK])))
+        } else {
+            Some(Self(DynDimInner::Heap(vec![0; rank])))
+        }
+    }
+
+    fn from_dimension<D>(other: &D) -> Option<Self>
+    where
+        D: Dimension,
+    {
+        Some(Self(DynDimInner::from_slice(other.as_slice())))
+    }
+
+    fn as_slice(&self) -> &[usize] {
+        self.0.deref()
+    }
+
+    fn as_mut_slice(&mut self) -> &mut [usize] {
+        self.0.deref_mut()
+    }
+}
+
+/// Multidimensional index into an array.
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
+pub struct ArrayIndex<D>(D);
+
+impl<D> ArrayIndex<D>
+where
+    D: Dimension,
+{
+    /// Creates a new multidimensional array index.
+    pub fn new(dim: D) -> Self {
+        Self(dim)
+    }
+
+    /// Returns a reference to the array index at the specified `DimIndex`.
+    pub fn get(&self, index: DimIndex) -> Option<usize> {
+        self.0.as_slice().get(index.0).copied()
+    }
+
+    /// Returns a slice containing all array indices.
+    pub fn as_slice(&self) -> &[usize] {
+        self.0.as_slice()
+    }
+}
+
+/// Shape of an array.
+///
+/// The entries represent the extent of the array along each dimension.
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
+pub struct Shape<D>(D);
+
+impl<D> Shape<D>
+where
+    D: Dimension,
+{
+    /// Creates a new array shape.
+    pub fn new(dim: D) -> Self {
+        Self(dim)
+    }
+
+    /// Returns a reference to the array extent at the specified `DimIndex`.
+    pub fn get(&self, index: DimIndex) -> Option<usize> {
+        self.0.as_slice().get(index.0).copied()
+    }
+
+    /// Returns a slice containing all array extents.
+    pub fn as_slice(&self) -> &[usize] {
+        self.0.as_slice()
+    }
+
+    /// Returns the product of array extents, or `None` if overflow occurred.
+    pub fn product_checked(&self) -> Option<usize> {
+        self.0.as_slice()
+            .iter()
+            .try_fold(1_usize, |acc, &d| acc.checked_mul(d))
+    }
+
+    /// Computes the contiguous strides from the array shape.
+    pub fn contiguous_strides(&self) -> Option<Strides<D>> {
+        let mut strides = D::zero(self.0.rank())
+            .expect("`D` can always represent its own rank");
+        if let Some(last) = strides.as_mut_slice().last_mut() {
+            let mut product = 1;
+            *last = product;
+            for (stride, &extent) in strides
+                .as_mut_slice()
+                .iter_mut()
+                .rev()
+                .skip(1)
+                .zip(self.0.as_slice().iter().rev())
+            {
+                product = product.checked_mul(extent)?;
+                *stride = product;
+            }
+        }
+
+        Some(Strides(strides))
+    }
+}
+
+/// Strides of the elements of an array.
+///
+/// The entries represent how far apart elements along a dimension are in the
+/// contiguous buffer.
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
+pub struct Strides<D>(D);
+
+impl<D> Strides<D>
+where
+    D: Dimension,
+{
+    /// Returns a reference to the element stride at the specified `DimIndex`.
+    pub fn get(&self, index: DimIndex) -> Option<usize> {
+        self.0.as_slice().get(index.0).copied()
+    }
+
+    /// Returns a slice containing all array element strides.
+    pub fn as_slice(&self) -> &[usize] {
+        self.0.as_slice()
+    }
+}
+
+/// Layout of an array.
+#[derive(Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug)]
+pub struct Layout<D> {
+    /// Array shape.
+    shape: Shape<D>,
+    /// Array strides.
+    strides: Strides<D>,
+    /// Offset from the start of the buffer.
+    offset: usize,
+    /// Element count of the layout.
+    len: usize,
+}
+
+impl<D> Layout<D>
+where
+    D: Dimension,
+{
+    /// Creates a contiguous layout with the given shape and offset.
+    ///
+    /// Returns `None` if [`Shape::product_checked`] returns `Some(0)` or `None`
+    /// or if no contiguous strides can be computed from `shape`.
+    pub fn contiguous(shape: Shape<D>, offset: usize) -> Option<Self> {
+        let len = shape.product_checked().filter(|&size| size != 0)?;
+        let strides = shape.contiguous_strides()?;
+
+        Some(Self {
+            shape,
+            strides,
+            offset,
+            len,
+        })
+    }
+
+    /// Returns a reference to the shape of the layout.
+    pub fn shape(&self) -> &Shape<D> {
+        &self.shape
+    }
+
+    /// Returns a reference to the strides of the layout.
+    pub fn strides(&self) -> &Strides<D> {
+        &self.strides
+    }
+
+    /// Returns the offset from the start of the buffer.
+    pub fn offset(&self) -> usize {
+        self.offset
+    }
+
+    /// Returns the number of elements contained in the layout.
+    pub fn len(&self) -> usize {
+        self.len
+    }
+
+    /// Returns `true` if the layout contains no elements.
+    ///
+    /// This always returns `false` for contiguous layouts.
+    pub fn is_empty(&self) -> bool {
+        self.len == 0
+    }
+
+    /// Largest buffer offset this layout can address.
+    ///
+    /// Returns `None` if overflow occurs.
+    pub fn max_offset(&self) -> Option<usize> {
+        self.shape.0
+            .as_slice()
+            .iter()
+            .zip(self.strides.0.as_slice())
+            .try_fold(self.offset, |acc, (&extent, &stride)| {
+                acc.checked_add((extent - 1).checked_mul(stride)?)
+            })
+    }
+}
