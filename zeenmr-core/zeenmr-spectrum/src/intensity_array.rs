2a060514423261ae99a788f7e2457b6c1ddf7a71
Sombke Maximilian <sombkemaximilian@gmail.com>
2026-08-13 09:00:18 +0200

    docs: numpy acknowledgement

--- zeenmr-core/zeenmr-spectrum/src/intensity_array.rs
+++ zeenmr-core/zeenmr-spectrum/src/intensity_array.rs
@@ -1,4 +1,17 @@
 //! Types and traits for representing spectrum intensities.
+//!
+//! # Design & Acknowledgement
+//!
+//! The multidimensional array implementation featured in this module is heavily
+//! inspired by [numpy]. While there are equivalent Rust crates, they, much like
+//! numpy itself, support a lot of features we simply do not need and/or are
+//! in their own unstable phase. Further, NMR and spectroscopy in general
+//! have quite specific algorithmic needs, letting us aggressively optimize
+//! the cases we care about while putting much less priority on some features
+//! that a general purpose multidimensional array implementation would have to
+//! worry about.
+//!
+//! [numpy]: https://numpy.org/
 
 mod array;
 pub use array::{
